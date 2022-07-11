273b3c22593cfe833e2cf8441c05fe55172f4b76
Wishnoo <rwishnoo@gmail.com>
2022-07-11 00:30:07 -0400

    Annotation example

---
+++ spring-demo-annotations/src/com/demo/springdemo/Coach.java
@@ -0,0 +1,6 @@
+package com.demo.springdemo;
+
+public interface Coach {
+	
+	public String getDailyWorkout();
+}
---
+++ spring-demo-annotations/src/com/demo/springdemo/TennisCoach.java
@@ -0,0 +1,13 @@
+package com.demo.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component("thatSillyCoach")
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley";
+	}
+
+}
