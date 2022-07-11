28a48bb55cdebaafa508cbceb6bdb38de0ff61da
Wishnoo <rwishnoo@gmail.com>
2022-07-11 15:15:27 -0400

    Annotation - field injection - @Qualifier

--- spring-demo-annotations/src/com/demo/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/demo/springdemo/TennisCoach.java
@@ -1,12 +1,14 @@
 package com.demo.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 	
 	@Autowired
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 	
 	// default constructor
