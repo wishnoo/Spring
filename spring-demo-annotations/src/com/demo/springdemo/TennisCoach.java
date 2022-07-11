0f8562b2958fcdca23e58e37c48543dd4367ac99
Wishnoo <rwishnoo@gmail.com>
2022-07-11 14:43:27 -0400

    Annotation - Setter Injection - without use of setter method

--- spring-demo-annotations/src/com/demo/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/demo/springdemo/TennisCoach.java
@@ -15,8 +15,8 @@ public class TennisCoach implements Coach {
 	
 	//define setter method
 	@Autowired
-	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println(">> TennisCoach: inside setFortuneService() method");
+	public void doSomeCrazyStuff(FortuneService fortuneService) {
+		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
 		this.fortuneService = fortuneService;
 	}
 	
