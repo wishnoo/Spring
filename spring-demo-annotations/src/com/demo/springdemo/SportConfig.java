0123ce5356519afd8f3017627dbe3ce9728dd891
Wishnoo <rwishnoo@gmail.com>
2022-07-13 20:12:32 -0400

    Configuration with Java instead of XML - used ComponentScan

---
+++ spring-demo-annotations/src/com/demo/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.demo.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.demo.springdemo")
+public class SportConfig {
+
+}
