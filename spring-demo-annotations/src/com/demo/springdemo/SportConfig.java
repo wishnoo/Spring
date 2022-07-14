0c3475d25f154ecacd30a2a5ec013c5e1ddddf32
Wishnoo <rwishnoo@gmail.com>
2022-07-13 21:56:07 -0400

    Configuration with Java - inject from properties

--- spring-demo-annotations/src/com/demo/springdemo/SportConfig.java
+++ spring-demo-annotations/src/com/demo/springdemo/SportConfig.java
@@ -3,9 +3,11 @@ package com.demo.springdemo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 //@ComponentScan("com.demo.springdemo")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
 
 	// define bean for our sad fortune service
--- spring-demo-annotations/src/com/demo/springdemo/SwimJavaConfigDemoApp.java
+++ spring-demo-annotations/src/com/demo/springdemo/SwimJavaConfigDemoApp.java
@@ -10,12 +10,17 @@ public class SwimJavaConfigDemoApp {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 		
 		// get the bean from spring container
-		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 		
 		// call a method on bean
 		System.out.println(theCoach.getDailyWorkout());
 		
 		System.out.println(theCoach.getDailyFortune());
+		
+		// call our new swim coach methods which has the properties injected.
+		System.out.println(theCoach.getEmail());
+		
+		System.out.println(theCoach.getTeam());
 		
 		// close the context
 		context.close();
