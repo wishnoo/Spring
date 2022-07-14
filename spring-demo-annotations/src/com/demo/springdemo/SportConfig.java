fe06e2ddf50af10b006a582e99fc9d8188423d39
Wishnoo <rwishnoo@gmail.com>
2022-07-13 20:46:53 -0400

    Java Configuration - Bean definition - example

--- spring-demo-annotations/src/com/demo/springdemo/SportConfig.java
+++ spring-demo-annotations/src/com/demo/springdemo/SportConfig.java
@@ -1,10 +1,22 @@
 package com.demo.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.demo.springdemo")
+//@ComponentScan("com.demo.springdemo")
 public class SportConfig {
 
+	// define bean for our sad fortune service
+	@Bean
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+	}
+	
+	// define bean for our swim coach and inject dependency
+	@Bean
+	public Coach swimCoach() {
+		return new SwimCoach(sadFortuneService());
+	}
 }
---
+++ spring-demo-annotations/src/com/demo/springdemo/SwimJavaConfigDemoApp.java
@@ -0,0 +1,24 @@
+package com.demo.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SwimJavaConfigDemoApp {
+
+	public static void main(String[] args) {
+
+		// read the spring config file
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+		
+		// get the bean from spring container
+		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		
+		// call a method on bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		System.out.println(theCoach.getDailyFortune());
+		
+		// close the context
+		context.close();
+	}
+
+}
