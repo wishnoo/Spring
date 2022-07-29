c8e16be9e256ddda00a351b36ceddafd53f13e95
Wishnoo <rwishnoo@gmail.com>
2022-07-29 13:35:25 -0400

    @RequestParam annotation

--- spring-mvc-demo/src/com/wishnoo/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/wishnoo/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -31,6 +32,21 @@ public class HelloWorldController {
 		
 		// convert the data to all caps
 		theName = "Yo! " + theName;
+		
+		// create the message
+		model.addAttribute("message", theName);
+		
+		// add message to the model
+		return "helloworld";
+	}
+	
+	// Request Mapping annotation
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName, Model model) {
+		
+		// convert the data to all caps
+		theName = "Hey My Friend from Version 3! " + theName;
 		
 		// create the message
 		model.addAttribute("message", theName);
