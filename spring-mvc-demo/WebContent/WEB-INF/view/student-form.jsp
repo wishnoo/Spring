8c84dac14d8bb13433e0f25df34c8d77de3b1dab
Wishnoo <rwishnoo@gmail.com>
2022-07-30 13:15:57 -0400

    Read properties file - inject into map in a class using #

--- spring-mvc-demo/WebContent/WEB-INF/view/student-form.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/student-form.jsp
@@ -22,10 +22,7 @@
 		
 		<form:select path="country">
 		
-			<form:option value="Brazil" label="Brazil"></form:option>
-			<form:option value="France" label="France"></form:option>
-			<form:option value="Germany" label="Germany"></form:option>
-			<form:option value="India" label="India"></form:option>
+			<form:options items="${student.countryOptions}"/>
 			
 		</form:select>
 		
--- spring-mvc-demo/src/com/wishnoo/springdemo/mvc/Student.java
+++ spring-mvc-demo/src/com/wishnoo/springdemo/mvc/Student.java
@@ -1,12 +1,26 @@
 package com.wishnoo.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
 	private String firstName;
 	private String lastName;
+	
 	private String country;
 	
-	public Student() {}
+	private LinkedHashMap<String,String> countryOptions;
+	
+	public Student() {
+		// populate country options : used ISO country code
+		countryOptions = new LinkedHashMap<>();
+		
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India");
+		countryOptions.put("US", "United States of America");
+	}
 
 	public String getFirstName() {
 		return firstName;
@@ -31,4 +45,10 @@ public class Student {
 	public void setCountry(String country) {
 		this.country = country;
 	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
+	
+	
 }
