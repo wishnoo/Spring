f1d70b8937555cf2732b3ddb080515cb6872c665
Wishnoo <rwishnoo@gmail.com>
2022-07-31 23:53:07 -0400

    Form validation - required fields

---
+++ spring-mvc-demo/src/com/wishnoo/springdemo/mvc/CustomerController.java
@@ -0,0 +1,37 @@
+package com.wishnoo.springdemo.mvc;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@RequestMapping("/showForm")
+	public String showForm(Model theModel) {
+		
+		// Add customer object to the model
+		theModel.addAttribute("customer", new Customer());
+		
+		// return the form page
+		return "customer-form";
+	}
+	
+	@RequestMapping("processForm")
+	public String processForm(
+			@Valid @ModelAttribute("customer") Customer customer,
+			BindingResult theBindingResult) {
+		
+		if(theBindingResult.hasErrors()) {
+			return "customer-form";
+		}
+		else {
+			return "customer-confirmation";
+		}
+	}
+}
