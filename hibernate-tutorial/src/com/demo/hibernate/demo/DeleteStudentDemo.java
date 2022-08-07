e748972d2937eea4f796bf0f9eeec701fa4f9cc6
Wishnoo <rwishnoo@gmail.com>
2022-08-07 19:11:26 -0400

    Hibernate Delete - query

--- hibernate-tutorial/src/com/demo/hibernate/demo/DeleteStudentDemo.java
+++ hibernate-tutorial/src/com/demo/hibernate/demo/DeleteStudentDemo.java
@@ -32,8 +32,12 @@ public class DeleteStudentDemo {
 			Student myStudent = session.get(Student.class, studentId);
 			
 			// delete the student
-			System.out.println("Deleting Student: " + myStudent);
-			session.delete(myStudent);
+			// System.out.println("Deleting Student: " + myStudent);
+			// session.delete(myStudent);
+			
+			// delete student id=2
+			System.out.println("Deleting student id=2");
+			session.createQuery("delete from Student where id=2").executeUpdate();
 			
 			// commit the transaction
 			session.getTransaction().commit();
